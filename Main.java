0eb016d475aaabd809f97d7b8aca460cdbab491a
austinl1905 <114597547+austinl1905@users.noreply.github.com>
2022-12-15 13:39:25 +0000

    add Jbutton

--- Main.java
+++ Main.java
@@ -1,43 +1,58 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
 import javax.swing.JTextPane;
 
 public class Main {
-    private static void createAndShowGUI() {
-        JFrame jFrame = new JFrame("Java Calculator");
-        jFrame.setLayout(new FlowLayout());
-        jFrame.setSize(500, 360);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  private static void createAndShowGUI() {
+    JFrame jFrame = new JFrame("Java Calculator");
+    jFrame.setLayout(new FlowLayout());
+    jFrame.setSize(500, 360);
+    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        ImageIcon calculatorImg = new ImageIcon("imgs/calculator.jpg");
-        Image image = calculatorImg.getImage();
-        Image newimg = image.getScaledInstance(50, 50,  java.awt.Image.SCALE_SMOOTH);
-        calculatorImg = new ImageIcon(newimg);
+    ImageIcon calculatorImg = new ImageIcon("imgs/calculator.jpg");
+    Image image = calculatorImg.getImage();
+    Image newimg = image.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
+    calculatorImg = new ImageIcon(newimg);
 
-        JLabel label = new JLabel(calculatorImg);
-        Border border = BorderFactory.createLineBorder(Color.BLACK);
-        label.setBorder(border);
-        label.setPreferredSize(new Dimension(150, 100));
+    JLabel label = new JLabel(calculatorImg);
 
-        label.setText("Java Calculator");
-        label.setVerticalTextPosition(SwingConstants.BOTTOM);
-        label.setHorizontalTextPosition(SwingConstants.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.CENTER);
+    Border lineBorder = BorderFactory.createLineBorder(Color.BLACK);
+    Border emptyBorder = BorderFactory.createEmptyBorder(0, 0, 0, 0);
+    Border compoundBorder = new CompoundBorder(lineBorder, emptyBorder);
 
-        JTextPane textPane = new JTextPane();
-        textPane.setEditable(false);
-        textPane.setText("An excuse for me to test math methods in Java lol");
-        textPane.setHorizontalAlignment(JTextPane.CENTER);
-        textPane.setVerticalAlignment(JTextPane.BOTTOM);
-        
-        
+    label.setBorder(compoundBorder);
+    label.setPreferredSize(new Dimension(150, 100));
+    label.setText("Java Calculator");
+    label.setVerticalTextPosition(SwingConstants.BOTTOM);
+    label.setHorizontalTextPosition(SwingConstants.CENTER);
+    label.setHorizontalAlignment(JLabel.CENTER);
+    label.setVerticalAlignment(JLabel.CENTER);
 
-        jFrame.add(label);
-        jFrame.setVisible(true);
-    }
+    JTextPane textPane = new JTextPane();
+    textPane.setEditable(false);
+    textPane.setText("An excuse for me to test math methods in Java lol");
+    textPane.setAlignmentX(0.5f);
+    textPane.setAlignmentY(0.5f);
+    textPane.setPreferredSize(new Dimension(200, 50));
+    textPane.setBorder(lineBorder);
+
+    JButton button = new JButton();
+    button.setText("Start");
+    button.setPreferredSize(new Dimension(50, 25));
+    button.setHorizontalAlignment(JLabel.CENTER);
+    button.setVerticalAlignment(JLabel.CENTER);
+    button.setBorder(lineBorder);
+ 
     
+    
+    jFrame.add(label);
+    jFrame.add(textPane);
+    jFrame.add(button);
+    jFrame.setVisible(true);
+  }
+
   public static void main(String[] args) {
     createAndShowGUI();
   }
