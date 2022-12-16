e0c05dc1ef6f154fd5bcdb7f1e8272efb1ea2f4e
austinl1905 <114597547+austinl1905@users.noreply.github.com>
2022-12-16 01:20:44 +0000

    add second frame

--- Main.java
+++ Main.java
@@ -3,19 +3,22 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.JTextPane;
+import javax.swing.text.*;
+import java.awt.event.*;
 
 public class Main {
-  private static void createAndShowGUI() {
+  private static void createAndShowCalculator() {
     JFrame jFrame = new JFrame("Java Calculator");
-    jFrame.setLayout(new FlowLayout());
+    jFrame.setLayout(new GridLayout(3, 3));
     jFrame.setSize(500, 360);
     jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+    
+    JPanel labelPanel = new JPanel(new FlowLayout());
     ImageIcon calculatorImg = new ImageIcon("imgs/calculator.jpg");
     Image image = calculatorImg.getImage();
     Image newimg = image.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
     calculatorImg = new ImageIcon(newimg);
-
+    
     JLabel label = new JLabel(calculatorImg);
 
     Border lineBorder = BorderFactory.createLineBorder(Color.BLACK);
@@ -29,31 +32,55 @@ public class Main {
     label.setHorizontalTextPosition(SwingConstants.CENTER);
     label.setHorizontalAlignment(JLabel.CENTER);
     label.setVerticalAlignment(JLabel.CENTER);
-
+    labelPanel.add(label);
+    
+    JPanel textPanePanel = new JPanel(new FlowLayout());
     JTextPane textPane = new JTextPane();
     textPane.setEditable(false);
     textPane.setText("An excuse for me to test math methods in Java lol");
-    textPane.setAlignmentX(0.5f);
-    textPane.setAlignmentY(0.5f);
-    textPane.setPreferredSize(new Dimension(200, 50));
+    StyledDocument doc = textPane.getStyledDocument();
+    SimpleAttributeSet center = new SimpleAttributeSet();
+    StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+    doc.setParagraphAttributes(0, doc.getLength(), center, false);
+    textPane.setPreferredSize(new Dimension(100, 75));
     textPane.setBorder(lineBorder);
-
+    textPanePanel.add(textPane);
+    
+    JPanel buttonPanel = new JPanel(new FlowLayout());
     JButton button = new JButton();
     button.setText("Start");
     button.setPreferredSize(new Dimension(50, 25));
     button.setHorizontalAlignment(JLabel.CENTER);
     button.setVerticalAlignment(JLabel.CENTER);
     button.setBorder(lineBorder);
- 
+    button.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        JFrame secondFrame = new JFrame("Java Calculator");
+        secondFrame.setSize(500, 360);
+        secondFrame.setLayout(new GridLayout(3, 3));
+        secondFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        secondFrame.setVisible(true);
+      }
+    });
+    buttonPanel.add(button);
     
+    jFrame.add(new JLabel());
+    jFrame.add(labelPanel);
+    for (int i = 0; i < 2; i++) {
+      jFrame.add(new JLabel());
+    }
+    jFrame.add(textPanePanel);
+     for (int i = 0; i < 2; i++) {
+      jFrame.add(new JLabel());
+    }
+    jFrame.add(buttonPanel);
+    jFrame.add(new JLabel());
     
-    jFrame.add(label);
-    jFrame.add(textPane);
-    jFrame.add(button);
     jFrame.setVisible(true);
   }
 
   public static void main(String[] args) {
-    createAndShowGUI();
+    createAndShowCalculator();
   }
 }
