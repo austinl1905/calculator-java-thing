b24c6e1061a538e4dc47f58c48e9c5307ff00648
austinl1905 <114597547+austinl1905@users.noreply.github.com>
2022-12-16 13:13:26 +0000

    add calculator (about time)

--- Main.java
+++ Main.java
@@ -37,7 +37,7 @@ public class Main {
     JPanel textPanePanel = new JPanel(new FlowLayout());
     JTextPane textPane = new JTextPane();
     textPane.setEditable(false);
-    textPane.setText("An excuse for me to test math methods in Java lol");
+    textPane.setText("don't use more than two operands at once I beg of you");
     StyledDocument doc = textPane.getStyledDocument();
     SimpleAttributeSet center = new SimpleAttributeSet();
     StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
@@ -57,9 +57,56 @@ public class Main {
       public void actionPerformed(ActionEvent e) {
         JFrame secondFrame = new JFrame("Java Calculator");
         secondFrame.setSize(500, 360);
-        secondFrame.setLayout(new GridLayout(3, 3));
+        secondFrame.setLayout(new FlowLayout());
         secondFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JPanel textFieldPanel = new JPanel(new FlowLayout());
+        JTextField textField = new JTextField();
+        textField.setPreferredSize(new Dimension(150, 20));
+        textFieldPanel.add(textField);
+
+        JPanel labelPanel = new JPanel(new FlowLayout());
+        JLabel outputLabel = new JLabel();
+        labelPanel.add(outputLabel);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        JButton getInputButton = new JButton("Submit");
+        getInputButton.setPreferredSize(new Dimension(100, 25));
+        getInputButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        String input = textField.getText();
+        // yeah I have no idea what goes on here either
+        String[] substrings = input.split("[+\\-*/\\^]");
+        
+        int operand1 = Integer.parseInt(substrings[0]);
+        int operand2 = Integer.parseInt(substrings[1]);
+        char operator = input.charAt(substrings[0].length());
+        int result = 0;
+        switch (operator) {
+          case '+':
+            result = operand1 + operand2;
+            break;
+          case '-':
+            result = operand1 - operand2;
+            break;
+          case '*':
+            result = operand1 * operand2;
+            break;
+          case '/':
+            result = operand1 / operand2;
+            break;
+          case '^':
+            result = (int) Math.pow(operand1, operand2);
+        }
+        outputLabel.setText(Integer.toString(result));
+      }
+    });
+        buttonPanel.add(getInputButton);
+        
+        secondFrame.add(textFieldPanel);
+        secondFrame.add(buttonPanel);
+        secondFrame.add(labelPanel);
+        
         secondFrame.setVisible(true);
       }
     });
